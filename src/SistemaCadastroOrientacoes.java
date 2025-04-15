fa975ac9a1e036ebc51d75458538d0fb5ead1ceb
Elis Jasper <elis_jasper@estudante.sesisenai.org.br>
2025-04-14 21:00:13 -0300

    Código inicial

---
+++ src/SistemaCadastroOrientacoes.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SistemaCadastroOrientacoes {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		
+		int decisao;
+		
+		System.out.println("-- Bem vindo(a) ao Sistema de Cadastro de Orientações WEG! --");
+		System.out.println(" ");
+		
+		System.out.println("1. Cadastrar Orientação");
+		System.out.println("2. Pesquisar Orientação");
+		System.out.println("3. Editar Orientação");
+		System.out.println("4. Excluir Orientação");
+		System.out.println("5. Sair (encerrar programa)");
+		
+		System.out.println("O que deseja realizar?");
+		decisao = leia.nextInt();
+		
+		switch(decisao) {
+		
+		case 1:
+			break;
+		
+		case 2:
+			break;
+			
+		case 3:
+			break;
+			
+		case 4:
+			break;
+		}
+		
+	}
+
+}
